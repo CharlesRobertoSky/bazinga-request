4815f9becdbdb9f400d43ba4fd38b2b05c77b498
CharlesRobertoSky <charlesdelimaroberto@gmail.com>
2025-03-20 18:47:54 -0300

    correcting slashbuild error

--- bot/Events/slashBuilder/builderSlashCommands.js
+++ bot/Events/slashBuilder/builderSlashCommands.js
@@ -1,17 +1,21 @@
 require('dotenv').config();
 const { REST, Collection, Routes } = require('discord.js');
-const fs = require('fs');
+const fs = require('node:fs');
 
 const ArraySlashs = [];
 const CollectionSlashs = new Collection();
 
 async function loadSlashCommands(path) {
+
   for (const file of fs.readdirSync(path)) {
+    console.log(fs.lstatSync(`${path}/${file}`).isDirectory())
     if (fs.lstatSync(`${path}/${file}`).isDirectory()) {
       loadSlashCommands(`${path}/${file}`);
     } else {
+      console.log(file.endsWith('.js'), 'WHERE')
       if (file.endsWith('.js')) {
-        const cmd = await import(`${path}/${file}`);
+        const cmd = await import(`../../../${path}/${file}`);
+        console.log(cmd)
         if (cmd.data && cmd.execute) {
           ArraySlashs.push(cmd.data);
           CollectionSlashs.set(cmd.data.name, cmd.execute);
@@ -22,7 +26,8 @@ async function loadSlashCommands(path) {
 }
 
 loadSlashCommands('./bot/Commands/');
-const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
+
+const rest = new REST({ version: '10' }).setToken("MTM0NjI4MDkxMTA4MzI3NDI5MA.GQKbut.lTPrn-BZ7h_jDemvwUA-cD20l-zkRkrFAAikmk");
 
 async function RegistrySlash(ID) {
   try {
--- bot/Handlers/eventHandler.js
+++ bot/Handlers/eventHandler.js
@@ -4,49 +4,52 @@ function loadEvents(client) {
   const fs = require('node:fs');
   const path = require('node:path');
 
-  // const folders = fs.readdirSync('./bot/Events');
+  const folders = fs.readdirSync('./bot/Events');
 
-  // for (const folder of folders) {
-  //   const files = fs
-  //     .readdirSync(`./bot/Events/${folder}`)
-  //     .filter(file => file.endsWith('.js'));
+  for (const folder of folders) {
+    const files = fs
+    .readdirSync(`./bot/Events/${folder}`)
+    .filter(file => file.endsWith('.js'));
+    console.log(files)
 
-  //   for (const file of files) {
-  //     const event = require(`../Events/${folder}/${file}`);
-  //     if (event.rest) {
-  //       if (event.once)
-  //         client.rest.once(event.name, (...args) =>
-  //           event.execute(...args, client)
-  //         );
-  //       else
-  //         client.rest.on(event.name, (...args) =>
-  //           event.execute(...args, client)
-  //         );
-  //     } else {
-  //       if (event.once)
-  //         client.once(event.name, (...args) => event.execute(...args, client));
-  //     }
-  //     table.addRow(file, 'loaded');
-  //     continue;
-  //   }
-  // }
-  const eventsPath = path.join(__dirname, './bot/Events');
-  const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
-
-  for (const file of eventFiles) {
-    console.log(eventFiles)
-	  const filePath = path.join(eventsPath, file);
-	  const event = require(filePath);
-	  if (event.once) {
-	  	client.once(event.name, (...args) => event.execute(...args));
-
-	  } else {
-	  	client.on(event.name, (...args) => event.execute(...args));
-    
-	  }
-    table.addRow(eventFiles, 'loaded');
-    
+    for (const file of files) {
+      const event = require(`../Events/${folder}/${file}`);
+      if (event.rest) {
+        if (event.once)
+          client.rest.once(event.name, (...args) =>
+            event.execute(...args, client)
+          );
+        else
+          console.log(client.rest)
+          client.rest.on(event.name, (...args) =>
+            event.execute(...args, client)
+          );
+      } else {
+        
+      }
+      table.addRow(file, 'loaded');
+      continue;
+    }
   }
+  // const eventsPath = path.join(__dirname, '../../bot/Events/');
+  // console.log(eventsPath)
+  // const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+  // console.log(eventFiles)
+
+  // for (const file of eventFiles) {
+  //   console.log(eventFiles)
+	//   const filePath = path.join(eventsPath, file);
+	//   const event = require(filePath);
+	//   if (event.once) {
+	//   	client.once(event.name, (...args) => event.execute(...args));
+
+	//   } else {
+	//   	client.on(event.name, (...args) => event.execute(...args));
+    
+	//   }
+  //   table.addRow(eventFiles, 'loaded');
+    
+  // }
   return console.log(table.toString(), '\n Loaded events');
 }
 module.exports = { loadEvents };
